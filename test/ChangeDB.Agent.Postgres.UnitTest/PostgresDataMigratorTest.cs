ab2c284ab43e184f4a147581dfd5805dd37c036e
pengbo.yang <obpt@sina.com>
2021-11-05 11:19:50 +0800

    add postgres agent testcase

---
+++ test/ChangeDB.Agent.Postgres.UnitTest/PostgresDataMigratorTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+using System.Threading.Tasks;
+using ChangeDB.Migration;
+using FluentAssertions;
+using Xunit;
+
+namespace ChangeDB.Agent.Postgres
+{
+    public class PostgresDataMigratorTest
+    {
+        [Fact]
+        public async Task ShouldReturnTableRowCountWhenCountTable()
+        {
+            var dataMigrator = PostgresDataMigrator.Default;
+            var rows = await  dataMigrator.CountTable(new TableDescriptor
+            {
+                 Name = "table1",
+                 Schema = "ts",
+            }, new MigrationSetting());
+           rows.Should().Be(3);
+        }
+        
+    }
+}
