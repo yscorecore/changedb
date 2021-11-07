e4eaf0b9afebe2c3f80ab6997d0fe34b9d1ec389
pengbo.yang <obpt@sina.com>
2021-11-07 17:02:28 +0800

    add console app

---
+++ src/ChangeDB.ConsoleApp/Commands/Migration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace ChangeDB.ConsoleApp.Commands
+{
+    [Verb("migration", HelpText = "Migration database from source to target")]
+    public class Migration:ICommand
+    {
+        [Value(1, MetaName = "source-dbtype", Required = true, HelpText = "source database type.")]
+        public string SourceType { get; set; }
+        [Value(2, MetaName = "source-connection", Required = true, HelpText = "source database connection strings")]
+        public string SourceConnectionString { get; set; }
+
+        [Value(3, MetaName = "target-dbtype", Required = true, HelpText = "target database type.")]
+        public string TargetType { get; set; }
+        [Value(4, MetaName = "target-connection", Required = true, HelpText = "target database connection strings")]
+        public string TargetConnectionString { get; set; }
+
+        public int Run()
+        {
+            return 0;
+        }
+    }
+}
---
+++ src/ChangeDB.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using CommandLine;
+
+namespace ChangeDB.ConsoleApp
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var commands = System.Reflection.Assembly.GetExecutingAssembly().GetTypes().Where(p => p.IsClass && typeof(ICommand).IsAssignableFrom(p)).ToArray();
+            return Parser.Default.ParseArguments(args, commands).MapResult(command => (command as ICommand).Run(), errors => 1);
+        }
+    }
+}
