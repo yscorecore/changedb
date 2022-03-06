c9ca378cf3cede3bd81d468e9bb88e7eb2f5b00a
chrisliang96 <chrisliang96@163.com>
2022-03-06 14:40:14 +0800

    Refactor help messages in Dump command

--- src/ChangeDB.ConsoleApp/Commands/DumpSql.cs
+++ src/ChangeDB.ConsoleApp/Commands/DumpSql.cs
@@ -6,54 +6,50 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace ChangeDB.ConsoleApp.Commands
 {
-    [Verb("dumpsql", HelpText = "Dump database as sql scripts")]
+    [Verb("dumpsql", HelpText = "Dump source database as target database sql scripts")]
     internal class DumpSql : BaseCommand
     {
         public override string CommandName { get => "dumpsql"; }
 
-        [Value(1, MetaName = "source-dbtype", Required = true, HelpText = "source database type. (mysql/postgres/sqlserver/sqlce)")]
+        [Value(1, MetaName = "Source-DBType", Required = true, HelpText = "Enter the type of input database. (ej:mysql/postgres/sqlserver/sqlce)")]
         public string SourceType { get; set; }
 
-        [Value(2, MetaName = "source-connection", Required = true, HelpText = "source database connection strings")]
+        [Value(2, MetaName = "Source-DBConnection", Required = true, HelpText = "Enter the source database connection strings, you can get help from ChangeDB Readme page")]
         public string SourceConnectionString { get; set; }
 
-        [Value(3, MetaName = "target-dbtype", Required = true, HelpText = "target database type, default use source database type. (mysql/postgres/sqlserver/sqlce)")]
+        [Value(3, MetaName = "Target-DBtype", Required = true, HelpText = "Enter the type of output database, ChangeDB will same database type as INPUT if output DBType isn't specified. (ej:mysql/postgres/sqlserver/sqlce)")]
         public string TargetType { get; set; }
 
 
-        [Value(4, MetaName = "output-file", Required = true, HelpText = "output script file")]
+        [Value(4, MetaName = "Output-FilePath", Required = true, HelpText = "Enter the file path to saved transformed sql scripts")]
         public string TargetScript { get; set; }
 
-        [Option('f', "force", HelpText = "drop target sql script file if exists", Default = false)]
+        [Option('f', "force", HelpText = "Warning:This option will DROP target sql script file if exists in path", Default = false)]
         public bool DropTargetDatabaseIfExists { get; set; } = false;
 
-        [Option("dump-scope", HelpText = "(All/Metadata/Data)", Default = MigrationScope.All)]
+        [Option("Dump-Option", HelpText = "Identify which part of database you want to transform. (ej:All/Metadata/Data)", Default = MigrationScope.All)]
         public MigrationScope DumpScope { get; set; } = MigrationScope.All;
 
-        [Option("name-style", HelpText = "target object name style (Original/Lower/Upper).")]
+        [Option("Naming-Style", HelpText = "Identify the naming style of transformed database scripts. (ej:Original/Lower/Upper).")]
         public NameStyle NameStyle { get; set; } = NameStyle.Original;
-        [Option("max-fetch-bytes", HelpText = "max fetch bytes when read source database (unit KB), default value is 10 (100KB).")]
+        [Option("Max-Fetch-Capacity", HelpText = "Enter the max capacity when ChangeDB fetch data from source database, default value is 100 (100 here equals 100KB).")]
         public int MaxFetchBytes { get; set; } = 100;
 
-        [Option("post-sql-file",
-          HelpText = "post sql file, execute these sql script after the migration one-by-one.")]
+        [Option("Extra-ExecuteFile", HelpText = "Choose the sql file location, ChangeDB will execute choosen script after the migration.")]
         public string PostSqlFile { get; set; }
 
-        [Option("post-sql-file-split", Required = false, HelpText = "sql file split chars, default value is \"\"", Default = "")]
+        [Option("Scripts-Separator", Required = false, HelpText = "Enter the separator for transformed script, default seperator is \"\"", Default = "")]
         public string PostSqlSplit { get; set; } = "";
 
 
-        [Option("target-default-schema",
-            HelpText = "target database default schema.")]
+        [Option("Target-Default-Schema", HelpText = "target database default schema.")]
         public string TargetDefaultSchema { get; set; }
 
-        [Option("optimize-insertion",
-            HelpText = "optimize insertion script.", Default = true)]
+        [Option("Optimize-Scripts-Insertion", HelpText = "Set ture changedb sill optimize insertion script of transformed sql scripts.", Default = true)]
         public bool OptimizeInsertion { get; set; } = true;
 
 
-        [Option("hide-progress",
-            HelpText = "hide progress bar.", Default = false)]
+        [Option("Hide-Process-Indicator", HelpText = "Set ture console table will hide transfrom indicator while ChangeDB migrate database. default value is false.", Default = false)]
         public bool HideProgress { get; set; }
 
 
