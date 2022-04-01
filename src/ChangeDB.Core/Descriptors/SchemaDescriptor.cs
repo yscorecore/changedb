ac7c20bd874256725ff05b3defaa2f1be8d00b93
pengbo.yang <obpt@sina.com>
2022-04-01 18:07:39 +0800

    remove unused property in agent

--- src/ChangeDB.Core/Descriptors/SchemaDescriptor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChangeDB
-{
-    public class SchemaDescriptor : INameObject
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public bool IsDefault { get; set; }
-    }
-}
